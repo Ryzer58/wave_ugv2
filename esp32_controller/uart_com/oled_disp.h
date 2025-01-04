76636f0d768859da473d0e01966a5da96bd364c8
Ryzer58 <ryestar101@gmail.com>
2025-01-04 22:10:46 +0000

    Begin intial rework of display functionality. Adjust the bootscreen layout

--- esp32_controller/uart_com/oled_disp.h
+++ esp32_controller/uart_com/oled_disp.h
@@ -1,6 +1,10 @@
-/* Display - Integrated into the chassis is an SSD1306 oled i2c driven display. This is connected to the control board via
- * the i2c header. The monochromatic display provides a simple and quick way display core stats about the UGV.
+/* Display - Mounted into the UGV chassis is the SSD1306 oled display, communicating over i2c. This is connected via the i2c header on the controller board.
+ * The monochromatic display provides a simple and quick way display core stats about the UGV.
  */
+
+#ifndef OLED_DISP_H
+#define OLED_DISP_H
+
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -17,12 +21,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void bootScreen(){
   display.clearDisplay();
-  display.setTextSize(1);
+  display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  display.print(F("UGV control: V0.2"));
-  display.setCursor(0, 10);
-  display.print(F("Waiting for connection"));
+  display.print(F("UGV02"));
+  display.setTextSize(1);
+  display.setCursor(0, 24);
+  display.print(F("V0.6"));
+  display.setCursor(28, 24);
+  display.print(F("Wait on signal"));
   display.display();
 
 }
@@ -39,11 +46,31 @@ void displayPowerData(powerData *data){
   display.print(F("volts:"));
   display.setCursor(80,0);
   display.print(v_supply);
-  display.setCursor(0, 10);
+  display.setCursor(0, 8);
   display.print(F("Current(mA):"));
-  display.setCursor(75,10);
+  display.setCursor(75,8);
   display.print(curr_supply);
   display.display();
 
 }
 
+void clearRow(int row){
+  int cursor_val = 0;
+  if (row == 1){
+    cursor_val = 8;
+  }
+
+  else if (row == 2){
+    cursor_val = 16;
+  }
+
+  else if (row == 3){
+    cursor_val = 24;
+  }
+
+  display.setCursor(0, cursor_val);
+  display.print(F("                    "));
+}
+ /* Todo - consider the best approuch for integrating temperature sensor data into our display data*/
+
+#endif
