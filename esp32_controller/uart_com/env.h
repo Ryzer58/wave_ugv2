7d74173b376a15003ba20bc08606ab82f73a6132
Ryzer58 <ryestar101@gmail.com>
2025-04-27 22:01:35 +0100

    Move setup function from main file into env.h

--- esp32_controller/uart_com/env.h
+++ esp32_controller/uart_com/env.h
@@ -1,4 +1,4 @@
-/* Temperature readings are not to far off by about 2 - 3 degrees, unlike the bme it does not measure humidity */
+/* Temperature readings appear to be a few degrees higher (3 - 4*C) than ambient temperature. Compared to the BME280 it does not measure humidity */
 
 #ifndef ENV_H
 #define ENV_H
@@ -10,10 +10,28 @@ Adafruit_BMP280 bmp;
 float ugv_temp;
 float ugv_pressure;
 
+bool probeBMP(int dev){
+  unsigned bmpReady;
+  bmpReady = bmp.begin(dev);
+  
+  if(!bmpReady){
+    return false;
+  }
+
+  else{
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    return true;
+  }
+}
+
 void fetchBmp280Data(){
 
   float raw_value;
-  float offset = 4.5; //based off of home thermostat readings
+  float offset = 4.5; // based off of home thermostat readings
   raw_value = bmp.readTemperature();
   ugv_temp = raw_value - offset;
 
@@ -30,5 +48,7 @@ void printBmp280Data(){
   Serial.println(" Pa");
   
 }
+
+// Altitude can be calculated but not currently implemented
 
 #endif
